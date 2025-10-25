c5ecaa702b3118df2fb3a97b63c27d2a7daf6134
Gabrie-ux <gabrielrimachi214@gmail.com>
2025-10-25 09:55:15 -0500

    Actualizar main-allfake .js

--- plugins/main-allfake .js
+++ plugins/main-allfake .js
@@ -9,7 +9,7 @@ let handler = async (m, { text, conn }) => {
 
 
   if (!text) {
-    return await conn.reply(m.chat, `🩵 Escribe el prompt de la imagen. Ejemplo:\n#dalle2 makima con denji.`, m)
+    return await conn.reply(m.chat, `🩵 Escribe el prompt de la imagen. Ejemplo:\n#dalle2 ISAGI YOICHI.`, m)
   }
 
   await conn.reply(m.chat, `🩵 Generando la imagen de: "${text}", espera un momento...`, m)
