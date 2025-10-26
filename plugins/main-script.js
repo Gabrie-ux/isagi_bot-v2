1926b46b3ef85c751ea4d8a0bcb9e61e8b62cfe7
Gabrie-ux <gabrielrimachi214@gmail.com>
2025-10-26 18:10:26 -0500

    Actualizar main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -1,13 +1,13 @@
 const handler = async (m, { conn }) => {
   const texto = `
- _*REPO DE LA BOT*_ 
+ _*REPO DE el BOT*_ 
 
 \`\`\`Repositorio OFC:\`\`\`
-https://github.com/mantis-has/Denji-Bot-MD- 
+https://github.com/Gabrie-ux/isagi_bot
 
 > 🌟 Deja tu estrella así nos motivas a seguir mejorando la bot.
 
-🔥 *Grupo oficial del bot:* https://chat.whatsapp.com/KoJjHo6o3Ew7P5qkjaIh0r
+🔥 *Grupo oficial del bot:* https://chat.whatsapp.com/KoJjHo6o3Ew7P5qkja
   `.trim()
 
   await conn.reply(m.chat, texto, m)
